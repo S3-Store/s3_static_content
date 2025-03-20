aad42bd50aee314444dcbe5ebf642447a9199f3a
Mayur Shah <shahmayur001@gmail.com>
2025-03-20 17:58:34 +0530

    Add routes for managing blogs
    Add routes for blogs, including CRUD operations and image deletion.
    This enables the necessary endpoints for blog management
    within the admin interface and API.

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,28 @@ Spree::Core::Engine.routes.draw do
         post :update_positions
       end
     end
+
+    resources :blogs do
+      collection do
+        post :update_positions
+      end
+
+      member do
+        delete :delete_image
+      end
+    end
   end
 
   # Check if the layout is not the default layout to avoid conflicts with the starter_frontend routes
   if Spree::Config.layout != 'layouts/storefront'
     constraints(SolidusStaticContent::RouteMatcher) do
       get '/(*path)', to: 'static_content#show', as: 'static'
+    end
+  end
+
+  if SolidusSupport.api_available?
+    namespace :api do
+      resources :blogs, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
